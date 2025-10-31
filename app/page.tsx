ac718c7c212d7c0b5f2a9a85b242b9e81a3f2463
ajitav.mishra <ajitav.mishra@imss.co.in>
2025-10-31 11:58:56 +0530

    nextJs

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind + TypeScript 🚀</h1>
+    </div>
+  );
+}
